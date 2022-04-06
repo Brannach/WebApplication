f404fa53c556a2b6cb4a39c491d7ea003fca0c3e
Marcelo Gomes de Oliveira <marcelo.oliveira@aquiris.com.br>
2022-04-06 16:14:04 -0300

    Creating repo

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: ProjectsController
+        public ActionResult Index()
+        {
+            ProjectContext context = HttpContext.RequestServices.GetService(typeof(Models.ProjectContext)) as ProjectContext;
+
+            return View(context.GetAllProjects());
+        }
+
+        // GET: ProjectsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProjectsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProjectsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            ProjectContext context = HttpContext.RequestServices.GetService(typeof(Models.ProjectContext)) as ProjectContext;
+            try
+            {
+                StringValues id;
+                StringValues name;
+                collection.TryGetValue("id", out id);
+                collection.TryGetValue("name", out name);
+                context.InsertNewProject(id, name);
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ProjectContext context = HttpContext.RequestServices.GetService(typeof(Models.ProjectContext)) as ProjectContext;
+            Project SelectedProject = context.GetProjectById(id);
+            return View(SelectedProject);
+        }
+
+        // POST: ProjectsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                ProjectContext context = HttpContext.RequestServices.GetService(typeof(Models.ProjectContext)) as ProjectContext;
+                StringValues name;
+                collection.TryGetValue("name", out name);
+                context.UpdateProject(new Project
+                    {
+                        id = id, 
+                        name = name
+                    }
+                );
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            ProjectContext context = HttpContext.RequestServices.GetService(typeof(Models.ProjectContext)) as ProjectContext;
+            Project SelectedProject = context.GetProjectById(id);
+            return View(SelectedProject);
+        }
+
+        // POST: ProjectsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/ProjectContext.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Primitives;
+using MySql.Data.MySqlClient;
+namespace WebApplication.Models
+{
+    public class ProjectContext
+    {
+        public string ConnectionString { get; set; }
+
+        public ProjectContext (string aConnectionString)
+        {
+            this.ConnectionString = aConnectionString;
+        }
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public List<Project> GetAllProjects()
+        {
+            List<Project> ProjectList = new List<Project>();
+
+            using (MySqlConnection MySqlConnection = GetConnection())
+            {
+                MySqlConnection.Open();
+                MySqlCommand MySqlCommand = new MySqlCommand("select * from project;", MySqlConnection);
+
+                using (var reader = MySqlCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        ProjectList.Add(new Project()
+                        {
+                            id = Convert.ToInt32(reader["id"]),
+                            name = reader["name"].ToString(),
+                        });
+                    }
+                }
+            }
+            return ProjectList;
+        }
+
+        public Project GetProjectById(int id)
+        {
+            Project AProject = new Project();
+
+            using (MySqlConnection MySqlConnection = GetConnection())
+            {
+                MySqlConnection.Open();
+                MySqlCommand MySqlCommand = new MySqlCommand("select * from project where id=@id;", MySqlConnection);
+                MySqlCommand.Parameters.AddWithValue("@id", id.ToString());
+                using (var reader = MySqlCommand.ExecuteReader())
+                {
+                    
+                    while (reader.Read())
+                    {
+                        AProject = new Project()
+                        {
+                            id = Convert.ToInt32(reader["id"]),
+                            name = reader["name"].ToString(),
+                        };
+                    }
+                }
+            }
+            return AProject;
+        }
+
+        public Project UpdateProject(Project NewValues)
+        {
+            Project AProjectList = new Project();
+
+            using (MySqlConnection MySqlConnection = GetConnection())
+            {
+                MySqlConnection.Open();
+                MySqlCommand MySqlCommand = new MySqlCommand("update project set name = @name where id=@id;", MySqlConnection);
+                MySqlCommand.Parameters.AddWithValue("@id", NewValues.id);
+                MySqlCommand.Parameters.AddWithValue("@name", NewValues.name);
+                using (var reader = MySqlCommand.ExecuteReader())
+                {
+
+                    while (reader.Read())
+                    {
+                        AProjectList = new Project()
+                        {
+                            id = Convert.ToInt32(reader["id"]),
+                            name = reader["name"].ToString(),
+                        };
+                    }
+                }
+            }
+            return AProjectList;
+        }
+
+        public void InsertNewProject(StringValues id, StringValues name)
+        {
+            using (MySqlConnection MySqlConnection = GetConnection())
+            {
+                var InsertStr = "insert into project (id, name) values(@id,@name);";
+                
+                using (MySqlCommand InsertCommand = new MySqlCommand(InsertStr, MySqlConnection))
+                {
+                    InsertCommand.Parameters.AddWithValue("@id", id.ToString());
+                    InsertCommand.Parameters.AddWithValue("@name", name.ToString());
+
+                    MySqlConnection.Open();
+                    InsertCommand.ExecuteNonQuery();
+                    MySqlConnection.Close();
+                }
+                
+            }
+        }
+    }
+}
